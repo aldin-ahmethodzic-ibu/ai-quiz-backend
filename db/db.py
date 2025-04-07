d170c065675a7a04c3f5dce8b9d52be8fad2d5ad
HermanAndrej <hermanandrej03@gmail.com>
2025-04-07 14:12:51 +0200

    db.py fix

--- db/db.py
+++ db/db.py
@@ -1,8 +1,10 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 from config import settings
+from models.user import User
+from models.quiz import Quiz
 
 async def init_db():
     client = AsyncIOMotorClient(settings.mongo_uri)
     db = client[settings.database_name]
-    await init_beanie(database=db, document_models=[])
+    await init_beanie(database=db, document_models=[User, Quiz])
